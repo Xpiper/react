1ebc0def652a111ee8067099d5c3e61665f5890b
apiper <apiper@126.com>
2022-01-13 00:27:00 +0800

    hello world

---
+++ hello-world/src/components/Greet.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// function Greet() {
+//     return (
+//         <h1> hello lpd</h1>
+//     );
+// }
+
+const Greet = () => <h1>hello lpd</h1>
+
+export default Greet;
