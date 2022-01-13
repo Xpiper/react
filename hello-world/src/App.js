f74c9d540758a7d614ae7da3a6d26a628c779a26
apiper <apiper@126.com>
2022-01-13 14:36:37 +0800

    Event Handling

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -4,15 +4,19 @@ import Welcome from "./components/Welcome";
 import Hello from "./components/Hello";
 import Message from "./components/Message";
 import Counter from "./components/Counter";
+import FunctionClick from "./components/FunctionClick";
+import ClassClick from "./components/ClassClick";
 
 function App() {
     return (
         <div className="App">
+            <FunctionClick/>
+            <ClassClick/>
             {/*<Counter/>*/}
             {/*<Message/>*/}
-            <Greet name='one' number={1}>
+            {/*<Greet name='one' number={1}>*/}
             {/*    <p>this is children props</p>*/}
-            </Greet>
+            {/*</Greet>*/}
             {/*<Greet name='two' number={2}/>*/}
             {/*<Greet name='three' number={3}/>*/}
             {/*<Welcome name='one' number={1}>*/}
