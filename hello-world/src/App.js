e5b0090f218fa657bbfe24ebcec3063f5bf3dd70
apiper <apiper@126.com>
2022-01-14 18:25:35 +0800

    Error Boundary

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -23,11 +23,20 @@ import RefsDemo from "./components/RefsDemo";
 import FocusInput from "./components/FocusInput";
 import FrParentInput from "./components/FRParentInput";
 import PortalDemo from "./components/PortalDemo";
+import Hero from "./components/Hero";
+import ErrorBoundary from "./components/ErrorBoundary";
 
 function App() {
     return (
         <div className="App">
-            <PortalDemo/>
+            <ErrorBoundary>
+                <Hero heroName='Batman'/>
+            </ErrorBoundary>
+            <ErrorBoundary>
+                <Hero heroName='Joker'/>
+            </ErrorBoundary>
+
+            {/*<PortalDemo/>*/}
             {/*<FrParentInput/>*/}
             {/*<FocusInput/>*/}
             {/*<RefsDemo/>*/}
